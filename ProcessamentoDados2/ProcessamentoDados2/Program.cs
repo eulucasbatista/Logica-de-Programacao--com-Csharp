caf7554b33b1e5e0c0ee665e9f448fb6fc19b8ba
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-10-13 14:47:24 -0300

    updates
    updates

--- ProcessamentoDados2/ProcessamentoDados2/Program.cs
+++ ProcessamentoDados2/ProcessamentoDados2/Program.cs
@@ -9,9 +9,15 @@ namespace ProcessamentoDados2
         {
 
             int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
             
             Console.WriteLine("Você digitou: ");
             Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2);
+
+
         }
     }
 }
