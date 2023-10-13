f1b6f69547a50d9c0fc219a553133d95992b2de3
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-10-13 15:00:21 -0300

    updates
    updates

--- ProcessamentoDados2/ProcessamentoDados2/Program.cs
+++ ProcessamentoDados2/ProcessamentoDados2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace ProcessamentoDados2
 {
@@ -10,13 +11,23 @@ namespace ProcessamentoDados2
 
             int n1 = int.Parse(Console.ReadLine());
             char ch = char.Parse(Console.ReadLine());
-            double n2 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
             
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
             Console.WriteLine("Você digitou: ");
             Console.WriteLine(n1);
             Console.WriteLine(ch);
-            Console.WriteLine(n2);
-
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.Write(altura.ToString("F2", CultureInfo.InvariantCulture));
 
         }
     }
