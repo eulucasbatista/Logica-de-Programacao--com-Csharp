140cfaf4f02a177cf0a48239d3a96b36cec4b647
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-10-13 18:34:46 -0300

    updates
    UPDATES

--- Exercicio1/Exercicio1/Program.cs
+++ Exercicio1/Exercicio1/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Diagnostics;
 using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Exercicio1
 {
@@ -8,23 +10,34 @@ namespace Exercicio1
     {
         static void Main(string[] args)
         {
-           
-            
-            string x;
-            int y;
-            double z;
+
             
             //Solicita o Nome Completo do Usuário
             //String vet usando o Split para o usuário poder escrever o nome completo por extenso
             Console.WriteLine("Entre com seu nome completo: ");
-            string[] vet = Console.ReadLine().Split(' ');
-            x = vet[0];
-
+            string nome = Console.ReadLine();
 
             //Pergunta quantos quarto tem na casa
             Console.WriteLine("Quantos quartos tem na sua casa: ");
-            y = int.Parse(Console.ReadLine());
+            int quartos = int.Parse(Console.ReadLine());
 
+            //Pede o preço de um produto
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double produto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Pede seu último nome, idade e altura
+            Console.WriteLine("Entre com seu último nome, idade e altura na mesma linda: ");
+            string[] vect = Console.ReadLine().Split(' ');
+            string ultimoNome = vect[0];
+            int age = int.Parse(vect[1]);
+            double height = double.Parse(vect[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(produto.ToString("F2", CultureInfo.InvariantCulture));
+            Console.Write(ultimoNome);
+            Console.Write(age);
+            Console.Write(height.ToString("F2", CultureInfo.InvariantCulture));
 
 
 
