a6f07214e7bf7c9cdffd70b8b3c785e7e65f593e
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-10-13 17:10:18 -0300

    Exercicio1
    Exercicio1

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            string x;
+            int y;
+            double z;
+            
+            //Solicita o Nome Completo do Usuário
+            //String vet usando o Split para o usuário poder escrever o nome completo por extenso
+            Console.WriteLine("Entre com seu nome completo: ");
+            string[] vet = Console.ReadLine().Split(' ');
+            x = vet[0];
+
+
+            //Pergunta quantos quarto tem na casa
+            Console.WriteLine("Quantos quartos tem na sua casa: ");
+            y = int.Parse(Console.ReadLine());
+
+
+
+
+        }
+    }
+}
