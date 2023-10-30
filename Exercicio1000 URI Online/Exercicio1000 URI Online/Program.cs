bde3e57fbf3b66725e695daec143a4a338c66522
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-10-30 12:05:15 -0300

    Exercicio100 URI
    Exercicio100 URI

---
+++ Exercicio1000 URI Online/Exercicio1000 URI Online/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio1000_URI_Online
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
