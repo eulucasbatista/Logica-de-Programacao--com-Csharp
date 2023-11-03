f807dafd1a2c6e871e601d429ae8225dac29b5c3
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-11-03 03:19:49 -0300

    Exercicios
    Exercicios

---
+++ Exercicio_1005_URI/Exercicio_1005_URI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio_1005_URI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double notaA, notaB;
+                       
+            notaA = Convert.ToDouble(Console.ReadLine());
+
+            notaB = Convert.ToDouble(Console.ReadLine());
+
+            // Aplicando os pesos e calculando a média
+            double media = ((notaA * 3.5) + (notaB * 7.5)) / 11.0;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5"));
+        }
+    }
+}
