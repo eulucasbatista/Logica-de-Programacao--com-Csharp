481e74de18d6a770c015d1674a8be7b17dad7a75
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-03-02 19:21:29 -0300

    New Exercicies
    Exercicio_1009_URI and Exercicio_1011_URI

---
+++ Exercicio_1009_URI/Exercicio_1009_URI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string vendedor;
+            double salario, totalvendas, salarioTotal;
+
+            vendedor = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalvendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salarioTotal = (totalvendas * 15.0 / 100) + salario;
+
+            Console.WriteLine("TOTAL = R$ " + salarioTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ Exercicio_1011_URI/Exercicio_1011_URI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valorRaio;
+            double volumeEsfera;
+            double pi = 3.14159;
+
+            valorRaio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            volumeEsfera = 4.0 / 3.0 * 3.14159 * valorRaio * valorRaio * valorRaio;
+
+            Console.WriteLine("VOLUME = " + volumeEsfera.ToString("F3", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
