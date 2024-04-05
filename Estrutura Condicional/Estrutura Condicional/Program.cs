001795ad08b59a441fa98c8a6c37d42ea9739ae1
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-04-05 09:52:53 -0300

    Estrutura Condicional
    Estrutura Condicional

---
+++ Estrutura Condicional/Estrutura Condicional/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Estrutura_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+
+        }
+    }
+}
