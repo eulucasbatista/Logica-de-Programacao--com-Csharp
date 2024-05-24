b08e08b169109f5b61ade982ff378ff5d39f6e7e
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-05-23 23:41:51 -0300

    Exercicio02EstruturaCondicional
    Exercicio02EstruturaCondicional

---
+++ Exericio02EstruturaCondicional/Exericio02EstruturaCondicional/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace Exercicio2 {
+        class Program {
+            static void Main(string[] args) {
+
+                double a, b, c, delta, r1, r2;
+
+                a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+                c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+                delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+                if (a == 0 || delta < 0.0) {
+                Console.WriteLine("Impossivel calcular");
+
+                }
+                 else{
+                   r1
+                 }
+                Console.ReadLine();
+            }
+        }
+}
