13968903ed74971c8d112ef4c398f60e5b77f0eb
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-05-27 10:43:11 -0300

    Exercicio3EstruturaCondicional
    Exercicio 3 Estrutura Condicional

---
+++ Exercicio3EstruturaCondicional/Exercicio3EstruturaCondicional/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+//Fazer um programa para ler três números inteiros.
+// Em seguida, mostrar qual o menor dentre os três números lidos
+// Em caso de empate, mostrar apenas uma vez
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            String[] vet = Console.ReadLine().Split
+
+
+
+            Console.ReadLine();
+        }
+       
+    }
+}
