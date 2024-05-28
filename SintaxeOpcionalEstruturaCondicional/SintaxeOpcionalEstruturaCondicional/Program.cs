54dd65f7c12c4adbb23519a590f5968bface48bc
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-05-28 15:37:31 -0300

    SintaxeOpcional
    SintaxeOpcional

---
+++ SintaxeOpcionalEstruturaCondicional/SintaxeOpcionalEstruturaCondicional/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100)
+            {
+                conta = conta + (minutos - 100) * 2.0;
+                Console.WriteLine("Valor a pagar: " + conta);
+            }
+
+        }
+    }
+}
