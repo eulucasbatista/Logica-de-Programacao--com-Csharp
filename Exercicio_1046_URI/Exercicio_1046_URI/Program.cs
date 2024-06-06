c44e0066dc465d0e5fd8c986fe7377b76d958811
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-06-06 19:38:14 -0300

    Exericio_1046_URI
    Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
    
    Entrada
    A entrada contém dois valores inteiros representando a hora de início e a hora de fim do jogo.
    
    Saída
    Apresente a duração do jogo conforme exemplo abaixo.

---
+++ Exercicio_1046_URI/Exercicio_1046_URI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Exercicio1046 { 
+    class URI
+    {
+        static void Main(string[] args)
+        {
+
+            String[] vet = Console.ReadLine().Split(' ');
+
+            int HoraInicio = int.Parse(vet[0]);
+            int HoraFim = int.Parse(vet[1]);
+
+            int duracao;
+
+            if (HoraInicio < HoraFim)
+            {
+                duracao = HoraFim - HoraInicio;
+
+            }
+            else
+            {
+                duracao = 24 - HoraInicio + HoraFim;
+            }
+
+            if (duracao == 0)
+            {
+                duracao = 24;
+            }
+            
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");   
+            
+         }
+    }
+
+}
