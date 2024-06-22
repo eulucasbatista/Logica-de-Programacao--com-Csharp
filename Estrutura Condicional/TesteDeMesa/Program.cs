29693fbbba09b9492d16b92c7f44232a00230be5
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-06-22 18:47:38 -0300

    Estrutura Condicional - Teste de Mesa
    Estrutura Condicional - Teste de Mesa

---
+++ Estrutura Condicional/TesteDeMesa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, y;
+            x = 0;
+            y = 4;
+
+            while(x < 3)
+            {
+                y = y + 2;
+                x = x + 1;
+                Console.WriteLine(x + "-" + y);
+            }
+
+        }
+    }
+}
--- Estrutura Condicional/TesteDeMesa/obj/Debug/net7.0/TesteDeMesa.AssemblyInfo.cs
+++ Estrutura Condicional/TesteDeMesa/obj/Debug/net7.0/TesteDeMesa.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("EstruturaRepetitiva")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TesteDeMesa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6c79d20ae5e083658efcaf9a6feb8226b89a99")]
-[assembly: System.Reflection.AssemblyProductAttribute("EstruturaRepetitiva")]
-[assembly: System.Reflection.AssemblyTitleAttribute("EstruturaRepetitiva")]
+[assembly: System.Reflection.AssemblyProductAttribute("TesteDeMesa")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TesteDeMesa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
