a828722c8b49b42c8d1e2ddad70f861cb3aa53dd
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-06-24 13:28:30 -0300

    Exerciccio_URI_1113
    Exerciccio_URI_1113

--- Exercicio_URI_1113/Program.cs
+++ Exercicio_URI_1113/Program.cs
@@ -4,11 +4,32 @@ namespace curso
 {
     class Program
     {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
 
             int x, y;
 
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
 
-            Console.ReadLine();
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Descrecente");
+                }
+               
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+
+            }
+
+        }
     }
 }
