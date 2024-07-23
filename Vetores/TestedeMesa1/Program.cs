854f72e85b2805d48229db80a351e143b23c8c23
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-07-22 23:34:07 -0300

    TestedeMesa1Vetor
    Teste de Mesa 1 Utilizando vetor

---
+++ Vetores/TestedeMesa1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestedeMesa
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+
+            int a, b, c;
+            int[] v = new int[4];
+
+            a = 10;
+            b = 20;
+            c = (a + b) / 2;
+            c = c - 40;
+            v[0]= a + b + c;
+
+            Console.WriteLine(v[0]);
+
+        }
+    }
+}
--- Vetores/TestedeMesa1/obj/Debug/net7.0/TestedeMesa1.AssemblyInfo.cs
+++ Vetores/TestedeMesa1/obj/Debug/net7.0/TestedeMesa1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Vetores")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TestedeMesa1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a155f64671b8c21ca386a99dc5c9ab28947a22c6")]
-[assembly: System.Reflection.AssemblyProductAttribute("Vetores")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Vetores")]
+[assembly: System.Reflection.AssemblyProductAttribute("TestedeMesa1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TestedeMesa1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
