4de186bea26efddcb6cd38c20ba968acc24dd782
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-07-28 16:43:47 -0300

    Exercicio1Vetor
    Exercicio 1 Vetor

---
+++ Vetores/Exercicio1Vetor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1Vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+            double[] vet = new double[N];
+
+            double maiorValor = double.MinValue;
+            //Leitura dos valores
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+                vet[i] = double.Parse(s, CultureInfo.InvariantCulture);
+             
+                // Atualiza o maior valor se o valor atual for maior
+                if (vet[i] > maiorValor)
+                {
+                    maiorValor = vet[i];
+                }
+            }
+            // Exibição dos valores 
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(vet[i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+                
+            }
+            Console.WriteLine();
+            // Exibição do maior valor
+            Console.WriteLine("Maior valor: " + maiorValor.ToString("F1", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.ReadLine();
+         
+
+        }
+    }
+}
--- Vetores/Exercicio2VetorResolvido/obj/Debug/net7.0/Exercicio2VetorResolvido.AssemblyInfo.cs
+++ Vetores/Exercicio2VetorResolvido/obj/Debug/net7.0/Exercicio2VetorResolvido.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio2VetorResolvido")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d128a99e46a2bf775f01fd1987f19e2a67f0a073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf6eb077bda3c525f4a25c7c7a9f803e0722630")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio2VetorResolvido")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio2VetorResolvido")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
